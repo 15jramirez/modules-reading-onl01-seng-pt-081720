99e954b0f94b543702b307638177a36a50116461
Jocelyn  Ramirez <ramirez.jocelyn73@yahoo.com>
2020-10-01 01:52:03 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,2 +1,13 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
+
+class Kid 
+  extend Fancydance::ClassMethods 
+  include Fancydance::InstanceMethods 
+  attr_accessor :name 
+  
+  def initialize(name)
+    @name = name 
+  end
+  
+end
